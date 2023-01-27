679bb457aaf9c1dd36e95c6848333d2911f685a0
Paolo Rapuano <bettercallpaul07@gmail.com>
2023-01-27 15:45:22 +0100

    Completato con Bonus

---
+++ js/scripts.js
@@ -0,0 +1,77 @@
+const {createApp} = Vue;
+
+createApp ({
+
+  data() {
+
+    return {
+
+      currentSlide: 0,
+      slides: [
+        {
+            image: 'img/01.webp',
+            title: 'Marvel\'s Spiderman Miles Morale',
+            text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+        }, {
+            image: 'img/02.webp',
+            title: 'Ratchet & Clank: Rift Apart',
+            text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+        }, {
+            image: 'img/03.webp',
+            title: 'Fortnite',
+            text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+        }, {
+            image: 'img/04.webp',
+            title: 'Stray',
+            text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+        }, {
+            image: 'img/05.webp',
+            title: "Marvel's Avengers",
+            text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+        }
+    ],
+      autoplay: null
+    };
+  },
+
+  methods: {
+
+    prevSlide() {
+
+      if (this.currentSlide == 0) {
+        this.currentSlide = this.slides.length -1;
+      }
+
+      else {
+
+        this.currentSlide--;
+      }
+    },
+
+    nextSlide() {
+
+      if (this.currentSlide == this.slides.length -1) {
+
+        this.currentSlide = 0;
+      }
+      else {
+        this.currentSlide++;
+
+      }
+    },
+
+    hoverOnSlider(){
+      clearInterval(this.autoplay);
+
+    },
+
+    mouseLeaves(){
+      this.autoplay = setInterval(this.nextSlide, 3000);
+    }
+  },
+
+  mounted() {
+    this.autoplay = setInterval(this.nextSlide, 3000);
+  }
+
+}).mount(app);
